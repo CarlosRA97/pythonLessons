00ee401b8bad391f7ea63a61cc1ce606439910da
carlosra97 <carlosriveroaro7@gmail.com>
2016-07-22 14:42:54 +0000

    Clases Ventilador y Luces terminadas

--- luces.py
+++ luces.py
@@ -3,15 +3,14 @@ class Luces(object):
     luz_estado = False
     luz_potencia = 0
 
-    @property
     def get_potencia_luz(self):
         return self.luz_potencia
 
     def set_potencia_luz(self, potencia):
         self.luz_potencia = potencia
 
+    def get_luz_estado(self):
+        return self.luz_estado
 
-
-
-print Luces().get_potencia_luz
-print Luces().luz_potencia
+    def set_luz_estado(self, on_off):
+        self.luz_estado = on_off
